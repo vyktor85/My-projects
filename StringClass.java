2d883b5950023b31e655bcf00fb9050e73ea255a
vyktor85 <85866197+vyktor85@users.noreply.github.com>
2021-07-02 20:56:35 +0300

    Add files via upload

---
+++ StringClass.java
@@ -0,0 +1,16 @@
+package md.tekwill;
+
+public class StringClass {
+    public static void main (String[] args){
+        String s1="STRING";
+
+        System.out.println(s1.charAt(0));
+        System.out.println(s1.charAt(1));
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.charAt(4));
+        System.out.println(s1.charAt(5));
+
+
+    }
+}
